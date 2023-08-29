44f3842d9ea3db606242d52c522cd231f1fb0cac
kopticx <fdzk100@gmail.com>
2023-08-29 01:07:12 -0600

    (DbContext) => Tablas Comentarios y AutoresController

--- ApplicationDbContext.cs
+++ ApplicationDbContext.cs
@@ -7,9 +7,20 @@ public class ApplicationDbContext : DbContext
 {
   public ApplicationDbContext(DbContextOptions options) : base(options)
   {
-    
+  }
+
+  protected override void OnModelCreating(ModelBuilder modelBuilder)
+  {
+    base.OnModelCreating(modelBuilder);
+
+    //Usando el Api fluyenye para PK compuestas
+
+    // modelBuilder.Entity<AutorLibro>()
+    // .HasKey(al => new {al.AutorId, al.LibroId});
   }
 
   public DbSet<Autor> Autores { get; set; }
   public DbSet<Libro> Libros { get; set; }
+  public DbSet<Comentario> Comentarios { get; set; }
+  public DbSet<AutorLibro> AutoresLibros { get; set; }
 }
