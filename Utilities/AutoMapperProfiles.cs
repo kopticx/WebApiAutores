28905c9bc60ab34be8d8f3a3fff7e18aabf11e24
kopticx <fdzk100@gmail.com>
2023-08-29 01:09:45 -0600

    (AutoMapper) => Creación de Mapeos

---
+++ Utilities/AutoMapperProfiles.cs
@@ -0,0 +1,20 @@
+﻿using AutoMapper;
+using WebApiAutores.Entities;
+using WebApiAutores.Models;
+
+namespace WebApiAutores.Utilities;
+
+public class AutoMapperProfiles : Profile
+{
+  public AutoMapperProfiles()
+  {
+    CreateMap<AutorCreacionDTO, Autor>();
+    CreateMap<Autor, AutorDTO>();
+
+    CreateMap<LibroCreacionDTO, Libro>();
+    CreateMap<Libro, LibroDTO>();
+
+    CreateMap<ComentarioCreacionDTO, Comentario>();
+    CreateMap<Comentario, ComentarioDTO>();
+  }
+}
